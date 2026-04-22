cb03de0014d8a3d06ddd17541163d6016f842354
Phengsovanreach <phengsovanreach4@gmail.com>
2026-04-22 15:52:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
+import sys
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL") 
-PORT = int(os.getenv("PORT", 10000))
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+if not BOT_TOKEN:
+    print("ERROR: BOT_TOKEN is missing")
+    sys.exit(1)
+
+if not WEBHOOK_URL:
+    print("ERROR: WEBHOOK_URL is missing")
+    sys.exit(1)
